6fe86cbd2fb081e95ca54ee0028fdde17a0023e2
Harsh3215 <prajapatiharinandan281@gmail.com>
2024-11-07 18:55:08 +0530

    Add files via upload

---
+++ day 33 STICKBREAK.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int l,k;
+        scanf("%d%d",&l,&k);
+        if(l%k==0){
+            printf("0\n");
+        }
+        else{
+            printf("1\n");
+        }
+    }
+
+}
